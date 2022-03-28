3b47d1422d7f3c1a7721a5d90fd2b0c2482b08ff
NotClairePro <77887456+NotClairePro@users.noreply.github.com>
2022-03-28 09:22:13 +0200

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,34 +1,30 @@
-# from adafruit_servokit import ServoKit
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 45554  # The port used by the server
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.connect((HOST, PORT))
+print("Connection on {}".format(PORT))
 
 from Ctrls import Strt, Controls
 
 if __name__ == '__main__':
     Manette = Controls()
+    mode = 0
     while True:
         Strt(Manette)
-        Id_control = 0
-        if Manette.JL != [0, 0]:
-            Id_control = "100"
-        elif Manette.LSB:
-            Id_control = "300"
-        elif Manette.L1:
-            Id_control = "500"
-        elif Manette.L2:
-            Id_control = "700"
-        elif Manette.JR != [0, 0]:
-            Id_control = "200"
-        elif Manette.RSB:
-            Id_control = "400"
-        elif Manette.R1:
-            Id_control = "600"
-        elif Manette.R2:
-            Id_control = "800"
-        elif Manette.Opt:
-            Etat = 1
-        elif Manette.Share:
-            Etat = 2
-        elif Manette.Menu:
-            Etat = 0
-        # elif Manette.A:
+        if mode == 0: #bouger
+            func = "bougerListeServo"
 
-        print(Id_control)
+            if Manette.JL != [0,0]: #servo 0 et servo 2 = bras gauche
+                angles = JL
+                server.send("[func,[0,2], angles]".encode('UTF-8'))
+
+            if Manette.JR != [0,0]: #servo 0 et servo 2 = bras gauche
+                angles = JR
+
+                server.send("[func,[5,7], angles]".encode('UTF-8'))
+
+        else: #setPosition
+            print("boop"
